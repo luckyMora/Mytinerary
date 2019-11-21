9a1747b46c64dad29eb15a26fa6bd29e8fa1e17a
Rodrigo Morales <r.morales.ubiqum@gmail.com>
2019-11-21 11:05:42 +0100

    create signin page and delete unnecessary files

---
+++ src/SignIn.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+    state = {}
+    handleChange = (e) => {console.log(e)}
+    handleSubmit = (e) => {console.log(e)}
+    render() {
+        return (
+            <div className="contaier">
+                <form onSubmit={this-this.handleSubmit} action="" className="white">
+                <h5 className="grey-text text-darken-3">Sign In</h5>
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" onChange={this.handleChange}/>                    
+                </div>
+                <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" onChange={this.handleChange}/>                    
+                </div>
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-detph-0">Login</button>
+                </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn
